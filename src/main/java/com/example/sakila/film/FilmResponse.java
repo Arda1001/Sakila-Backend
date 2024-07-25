72b12ff7f6095e13ae5087a9a8901b8ed1ddf0c4
Arda Ordu <ardaordu03@gmail.com>
2024-07-25 12:12:53 +0100

    Update FilmResponse to return all the details about the film.

--- src/main/java/com/example/sakila/film/FilmResponse.java
+++ src/main/java/com/example/sakila/film/FilmResponse.java
@@ -1,22 +1,41 @@
 package com.example.sakila.film;
 
 import com.example.sakila.actor.PartialActorResponse;
+import com.example.sakila.language.Language;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.Set;
 
 @Getter
 public class FilmResponse {
     private final Short id;
     private final String title;
     private final int releaseYear;
+    private final String description;
+    private final int rentalDuration;
+    private final double rentalRate;
+    private final int length;
+    private final double replacementCost;
+    private final Rating rating;
+    private final Set<SpecialFeatures> specialFeatures;
+    private final Language languageId;
+    private final Language originalLanguageId;
     private final List<PartialActorResponse> cast;
 
     public FilmResponse(Film film) {
         this.id = film.getId();
         this.title = film.getTitle();
         this.releaseYear = film.getReleaseYear();
+        this.description = film.getDescription();
+        this.rentalDuration = film.getRentalDuration();
+        this.rentalRate = film.getRentalRate();
+        this.length = film.getLength();
+        this.replacementCost = film.getReplacementCost();
+        this.rating = film.getRating();
+        this.specialFeatures = film.getSpecialFeatures();
+        this.languageId = film.getLanguageId();
+        this.originalLanguageId = film.getOriginalLanguageId();
         this.cast = film.getCast().stream().map(PartialActorResponse::new).toList();
-
     }
 }
